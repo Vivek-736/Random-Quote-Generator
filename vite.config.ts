ebe10747c3492ac1b232f6676a9029293c52614a
Vivek <vivekousik8555@gmail.com>
2025-05-17 15:09:15 +0530

    dependencies updated and favicon added with meta data

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss]
+    }
+  }
 })
